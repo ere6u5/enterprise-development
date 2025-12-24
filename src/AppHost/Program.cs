8af88a77df4f0bde36e5f70b32b1492fee0e2535
Nick <nick.notset@mail.ru>
2025-12-24 18:41:48 +0400

    123123"

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,12 +1,13 @@
 using Microsoft.Extensions.Hosting;
 
 // var builder = DistributedApplication.CreateBuilder(args);
+var builder = DistributedApplication.CreateBuilder(args);
 
-var builder = DistributedApplication.CreateBuilder(new DistributedApplicationOptions
-{
-    Args = args,
-    DisableDashboard = true
-});
+// var builder = DistributedApplication.CreateBuilder(new DistributedApplicationOptions
+// {
+//     Args = args,
+//     DisableDashboard = true
+// });
 
 // Добавляем MySQL базу данных
 var mysql = builder.AddMySql("mysql")
@@ -18,6 +19,8 @@ var nats = builder.AddNats("nats");
 // Добавляем API проект
 var api = builder.AddProject<Projects.Api>("api")
     .WithReference(mysql)
-    .WithReference(nats);
+    .WithReference(nats)
+    .WaitFor(mysql) // Ждем пока база поднимется
+    .WaitFor(nats); // Ждем пока NATS поднимется
 
 builder.Build().Run();
